256d658c7ee91b4c3342f8b16755c682dd9681b9
Time <92392412+Shubh-Soft@users.noreply.github.com>
2022-08-11 15:37:29 +0300

    Add files via upload

---
+++ Javascript/other/script.js
@@ -0,0 +1,52 @@
+var myName = 'shubh';
+var z = 3;
+var y = 2;
+
+console.log(y+z);
+
+console.log(myName);
+
+var a = 4;
+var b = 2;
+
+console.log(4+2);
+console.log(4/2);
+console.log(4*2);
+console.log(4-2);
+
+var johnAge = 18;
+var bobAge = 16;
+
+console.log(johnAge+bobAge);
+
+
+var Name = 'John';
+var Age = 25;
+var Job = 'Business-Man';
+var Marry = 'not';
+
+console.log(Name  + ' is a ' + Age + ' year old ' + Job + ' he is ' + Marry + ' married ');
+
+alert(Name  + ' is a ' + Age + ' year old ' + Job + ' he is ' + Marry + ' married ');
+
+var age = 28;
+var name = 'john';
+var lastName = prompt('What is his last name?');
+console.log(Name + '' + lastName);
+
+
+/*******************************
+ * Basic Operators
+ */
+var year, yearJohn, yearMark;
+var now = 2018;
+var yearJohn = now - 28;
+var yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+
+console.log(now * 2);
+
+console.log(now / 10);
---
+++ Javascript/other/table.js
@@ -0,0 +1,7 @@
+const x = 3;
+
+for(i=1;i<=10;i++){
+    console.log(`\t ${x*i}`)
+}
+
+
